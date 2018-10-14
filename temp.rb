a955024521c5a8cc73d437864d0ff5ace6e02506
dwolfe1987 <dwolfe1987@gmail.com>
2018-10-14 15:53:08 +0000

    automatically backed up by learn

--- temp.rb
+++ temp.rb
@@ -1,18 +1,6 @@
 example_array = [1,2,3,4]
 
-def reverse_an_array(array)
-  
-  counter = log 
-  while coutner < array.length 
-  array[counter]
-  
-  counter += 1 
-end 
-  
-end
-
-reverse_an_array(example_array)
 
 
-array = [1,2,3,4]
- array.reverse 
+
+ 
