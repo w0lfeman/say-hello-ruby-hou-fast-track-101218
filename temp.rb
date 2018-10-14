67a1652c8d1f61310273f65db5ffca2c340bd81b
dwolfe1987 <dwolfe1987@gmail.com>
2018-10-14 15:47:47 +0000

    automatically backed up by learn

--- temp.rb
+++ temp.rb
@@ -1,9 +1,18 @@
-def fizzbuzz
-  x = l 
-  while x <= 100
-  puts x 
-  x += 1 
-end 
-end 
+example_array = [1,2,3,4]
 
-fizzbuzz
+def reverse_an_array(array)
+  
+  counter = log 
+  while coutner < array.length 
+  array[counter]
+  
+  counter += 1 
+end 
+  
+end
+
+reverse_an_array(example_array)
+
+
+array = [1,2,3,4]
+puts array.reverse 
